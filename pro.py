c40b900e76e1e2a20ac9eaa6cb97b0636d9e62ec
Sanskar Baluni <90570706+SanskarBaluni@users.noreply.github.com>
2022-07-16 13:05:48 +0530

    Add files via upload

---
+++ pro.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('ggplot')
+%matplotlib inline
+
+header=st.beta_container()
+dataset=st.beta_container()
+features=st.beta_container()
+
+
+import plotly
+import plotly.express as px
+import plotly.graph_objects as go
+plt.rcParams['figure.figsize']=17,8
+
+import cufflinks as cf
+import plotly.offline as pyo
+from plotly.offline import init_notebook_mode,plot,iplot
+
+import folium
+
+with header:
+    st.title('Health Dataset Visualization')
+with dataset:
+df=pd.read_excel(r"D:\Edge Download\Covid cases in India.xlsx")
+st.dataset(df)
